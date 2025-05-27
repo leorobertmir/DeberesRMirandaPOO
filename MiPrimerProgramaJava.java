af714a2f3a45b020997e8d37be19c7563915d01d
leorobertmir <romirsaec@gmail.com>
2025-05-27 14:20:48 -0500

    Add files via upload
    pag 23 ejercicio 2

---
+++ MiPrimerProgramaJava.java
@@ -0,0 +1,12 @@
+public class MiPrimerProgramaJava {
+
+    public static void main(String[] args) {
+        // 1. Crear una variable de tipo String (cadena de texto) llamada 'nombre'
+        // y asignarle tu nombre completo.
+        String nombre = "ROBERTO MIRANDA, ALUMNO POO 2025"; // ¡No olvides reemplazar esto con tu nombre!
+
+        // 2. Mostrar el valor de la variable 'nombre' por pantalla.
+        // System.out.println() se usa para imprimir texto y luego saltar a la siguiente línea.
+        System.out.println("Mi nombre es: " + nombre);
+    }
+}
